a7a66c1d71b129fcb9e7e35c18e3196a0fdc11e9
Alain O'Dea <alain.odea@gmail.com>
2008-07-05 21:07:32 -0230

    Added Mixed-type Completion

---
+++ src/tm_complete.erl
@@ -0,0 +1,33 @@
+-module (tm_complete).
+-export ([string/1, string/2]).
+
+string(String) -> snippet(users_choice(complete:string(String))).
+string(Local, String) -> snippet(users_choice(complete:string(Local, String))).
+
+users_choice([Option]) -> Option;
+users_choice(Options) ->
+    Menu = menu(Options),
+    option(tm_menu:selection(Menu), Options, Menu).
+
+menu([{fun_ref, {Function, Arity}}|Options]) ->
+    [lists:flatten(io_lib:format("~w/~w", [Function, Arity]))|menu(Options)];
+menu([{call, {Function, Arity}}|Options]) ->
+    [lists:flatten(io_lib:format("~w/~w", [Function, Arity]))|menu(Options)];
+menu([{module, Module}|Options]) -> [atom_to_list(Module)|menu(Options)];
+menu([_|Options]) -> menu(Options);
+menu([]) -> [].
+
+option([], _, _) -> [];
+option(Selection, [Option|_], [Selection|_]) -> Option;
+option(Selection, [_|Options], [_|Menu]) -> option(Selection, Options, Menu).
+
+snippet({fun_ref, {F, A}}) -> io_lib:format("~w/~w", [F, A]);
+snippet({call, {F, A}}) -> io_lib:format("~w(~s)", [F, args(A)]);
+snippet({module, M}) -> atom_to_list(M);
+snippet(_) -> "".
+
+args(Arity) -> args(Arity, 1).
+args(Arity, Arity) -> io_lib:format("${~w:Arg~w}", [Arity, Arity]);
+args(Arity, N) ->
+    [io_lib:format("${~w:Arg~w}, ", [N, N])|
+    args(Arity, N + 1)].
--- src/tm_menu.erl
+++ src/tm_menu.erl
@@ -1,24 +1,24 @@
 -module(tm_menu).
--export([get_selection/1]).
+-export([selection/1]).
 -include_lib("xmerl/include/xmerl.hrl").
 
-get_selection([]) -> "";
-get_selection(MenuItems) ->
+selection([]) -> "";
+selection(MenuItems) ->
     %% TODO: need better way to get path for tm_dialog
     PlistXml = os:cmd(io_lib:format("/Applications/TextMate.app/Contents/PlugIns/Dialog.tmplugin/Contents/Resources/tm_dialog -u -p \"~s\"", [ascii_plist(MenuItems)])),
     {PlistDoc, _Rest} = xmerl_scan:string(PlistXml),
-    [#xmlText{value=Selected}] = xmerl_xpath:string("/plist/dict/dict[preceding-sibling::key[1]='selectedMenuItem']/string[preceding-sibling::key[1]='title']/text()", PlistDoc),
-    Selected.
+    selected( xmerl_xpath:string("/plist/dict/dict[preceding-sibling::key[1]='selectedMenuItem']/string[preceding-sibling::key[1]='title']/text()", PlistDoc)).
+
+selected([#xmlText{value=Selected}]) -> Selected;
+selected([]) ->"".
 
 ascii_plist(MenuItems) ->
-    lists:flatten(
-        lists:reverse([");}"|
-            lists:foldl(
-                fun(Item, Completions) ->
-                    [io_lib:format("{title = ~s;},", [Item])|Completions]
-                end,
-                ["{menuItems = ("],
-                MenuItems
-            )
-        ])
-    ).
+    lists:reverse([");}"|
+        lists:foldl(
+            fun(Item, Completions) ->
+                [io_lib:format("{title = ~s;},", [Item])|Completions]
+            end,
+            ["{menuItems = ("],
+            MenuItems
+        )
+    ]).
