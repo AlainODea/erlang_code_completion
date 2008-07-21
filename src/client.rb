aca2f60a034975363abb348ea6715002083db220
Alain O'Dea <alain.odea@gmail.com>
2008-07-20 22:50:59 -0230

    BUGFIX: Canceling Menu Causes Relaunch of Completion Server
    - added clause for response of "[]" which happens if user cancels
    - reorganized client.rb to more clearly express intent

--- src/client.rb
+++ src/client.rb
@@ -12,11 +12,15 @@ to_complete = STDIN.readlines
 to_complete = to_complete[line_number-1][0..line_index-1].gsub('\\', '\\\\\\\\').gsub('"', '\"')
 erl_call=`find #{ENV['ERLANG_HOME']} -name erl_call`.split("\n")[0]
 response = `#{erl_call} -n tm_complete -a 'code add_path [\"#{ebin}\"]' -a 'tm_complete complete [#{module_name}, \"#{to_complete}\"]' 2>/dev/null`
-if (response =~ /^".*"$/) == nil
-    TextMate.call_with_progress(:title => "Erlang Code Completion", :message => "Launching Erlang Code Completion Server... Please wait") do
+if (response == "[]")
+    TextMate.exit_discard
+elsif (response =~ /^".*"$/)
+    TextMate.exit_insert_snippet(response[1..-2])
+else
+    TextMate.call_with_progress(:title => "Code Completion", :message => "Preparing Code Completion…") do
         `cd "#{support}/completion"; make --silent >/dev/null 2>&1`
         `#{erl_call} -s -n tm_complete -a 'code add_path [\"#{support}/completion/ebin\"]'`
     end
     response = `#{erl_call} -n tm_complete -a 'code add_path [\"#{ebin}\"]' -a 'tm_complete complete [#{module_name}, \"#{to_complete}\"]' 2>/dev/null`
+    TextMate.exit_insert_snippet(response[1..-2])
 end
-TextMate.exit_insert_snippet(response[1..-2])
